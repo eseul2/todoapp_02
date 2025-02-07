d707146ff84aede80838176df17473d4d4319ed7
eseul2 <dltmfsus@naver.com>
2025-02-07 12:15:11 +0900

    테일윈드 충돌 방지

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,14 @@
-import Image from "next/image";
+'use client';
+
+import Button from '@mui/material/Button';
 
 export default function Home() {
-  return <>안녕</>;
+  return (
+    <>
+      <button class="tw-bg-red-400">버튼</button>
+      <Button className="tw-bg-red-400" variant="contained">
+        Text
+      </Button>
+    </>
+  );
 }
